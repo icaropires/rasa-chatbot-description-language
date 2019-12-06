44304fdf01b88d7d4143910b6115487660c29465
joaorobson <joaorobsonmartins@gmail.com>
2019-12-06 15:13:12 -0300

    Add files to new structure
    Co-authored-by: Ícaro Aragão <icaropsa@gmail.com>

--- rasa_language/parser.py
+++ rasa_language/parser.py
@@ -1,2 +1,30 @@
+from lark import Lark, InlineTransformer
+
+class RasaTransformer(InlineTransformer):
+    
+    def start(self, blocks):
+        return ['blocks', blocks]
+
+    def blocks(self, *block):
+        return [*block]
+
+    def topics(self, *topic):
+        return ['topics', list(topic)]
+    
+    def topic(self, text):
+        return ['topic', str(text)]
+
+    def header(self, type_, name):
+        return ['header', [str(type_), str(name)]]
+
+def _make_grammar():
+    with open('grammar.lark') as fd:
+        grammar = Lark(fd, parser='lalr', transformer=RasaTransformer())
+    return grammar
+
 def parse(src):
-    ...
+    parser = _make_grammar()
+
+    return parser.parse(src)
+
+parse = _make_grammar()
