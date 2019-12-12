744c26457d79ffc5816b44d236b1f2246ac565b2
Ícaro Pires <icaropsa@gmail.com>
2019-12-12 16:26:10 -0300

    Add CLI command to parse and dump nlu file

--- rasa_language/__main__.py
+++ rasa_language/__main__.py
@@ -1,15 +1,27 @@
-from .parser import parse as parse_src
+import pathlib
+import json
 from sys import argv, exit
+
+import click
 from . import __version__ as version
+from .parser import parse as parse_src
+from .rasa_language import RasaLanguage
 
 
-def main():
-    if len(argv) <= 1:
-        print("Wrong Usage")
-        exit(1)
+@click.command()
+@click.argument("src_file", type=click.File("r"))
+@click.argument("bot_dir")
+def main(src_file, bot_dir):
+    lang = RasaLanguage()
+    lang.process(src_file.read())
 
-    with open(argv[1]) as input_txt:
-        ast = parse_src(input_txt.read())
+    path = pathlib.Path(bot_dir) / "data" / "nlu.json"
+
+    with open(path, "w") as f_out:
+        json.dump(
+            lang.nlu, f_out, ensure_ascii=False, indent=4, sort_keys=True
+        )
+        click.secho("NLU file generated succesfully!", fg="green", bold=True)
 
 
 if __name__ == "__main__":
