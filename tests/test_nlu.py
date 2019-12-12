f93853e28585d0d475134f164ad774e16ce0a156
Ícaro Pires <icaropsa@gmail.com>
2019-12-12 00:03:33 -0300

    Add test to nlu intent runtime

--- tests/test_nlu.py
+++ tests/test_nlu.py
@@ -1,8 +1,9 @@
-from rasa_language import parse
+import json
+from rasa_language import parse, eval_
 
 
 class TestNLU:
-    def test_intent(self):
+    def test_parse_intent(self):
         intent = "[intent: greet]\n"
         intent += "> hello\n"
         intent += "> hi\n"
@@ -19,3 +20,55 @@ class TestNLU:
         ]
 
         assert parse(intent) == expected
+
+    def test_run_intent(self):
+        intent = """[intent: star-wars]
+> lado negro da força
+> lado negro da forca
+> lado negro
+> frase de star wars
+> guerra nas estrelas
+> darth vader
+"""
+
+        nlu_json = {
+            "rasa_nlu_data": {
+                "common_examples": [
+                    {
+                        "text": "lado negro da força",
+                        "intent": "star-wars",
+                        "entities": [],
+                    },
+                    {
+                        "text": "lado negro da forca",
+                        "intent": "star-wars",
+                        "entities": [],
+                    },
+                    {
+                        "text": "lado negro",
+                        "intent": "star-wars",
+                        "entities": [],
+                    },
+                    {
+                        "text": "frase de star wars",
+                        "intent": "star-wars",
+                        "entities": [],
+                    },
+                    {
+                        "text": "guerra nas estrelas",
+                        "intent": "star-wars",
+                        "entities": [],
+                    },
+                    {
+                        "text": "darth vader",
+                        "intent": "star-wars",
+                        "entities": [],
+                    },
+                ],
+                "regex_features": [],
+                "lookup_tables": [],
+                "entity_synonyms": [],
+            }
+        }
+
+        assert eval_(parse(intent)) == nlu_json
