26b6ef0535d8673b5409e5b98854f18f9b1373d5
Ícaro Pires <icaropsa@gmail.com>
2019-12-13 03:46:53 -0300

    Add test for testing processing of story

--- tests/test_stories.py
+++ tests/test_stories.py
@@ -2,6 +2,29 @@ import pathlib
 
 
 class TestStories:
+    def test_stories_process(self, lang):
+        story = """[intent: star-wars]
+> Quero conselhos do yoda
+> Eu gostaria de conselhos do mestre
+
+[utter: star-wars]
+- O medo é o caminho para o lado negro.
+- Que a Força esteja com você!
+
+[star-wars]
+> Quero conselhos do yoda
+- O medo é o caminho para o lado negro.
+"""
+        expected = {
+            "star-wars": [
+                {"type": "intent", "name": "star-wars", "entities": {}},
+                {"type": "action", "name": "star-wars"},
+            ]
+        }
+
+        lang.process(story)
+        assert lang.stories == expected
+
     def test_stories_dump(self, lang, tmpdir):
         lang.stories = {
             "Star Wars": [
