0eaa8da38907e80be4a7f82f193c3681a679d193
Ícaro Pires <icaropsa@gmail.com>
2019-12-13 01:38:18 -0300

    Add test for utters and actions on domain

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -6,7 +6,7 @@ class TestDomain:
         expected = {
             "intents": ["greet"],
             "actions": [],
-            "templates": [],
+            "templates": {},
             "slots": [],
             "entities": [],
         }
@@ -15,11 +15,56 @@ class TestDomain:
 
         assert lang.domain == expected
 
+    def test_utter(
+        self, lang,
+    ):
+        utters = """[utter: star-wars]
+- O medo leva à raiva, a raiva leva ao ódio e o ódio leva ao sofrimento.
+- Que a Força esteja com você!
+- Grande guerreiro? Guerra não faz grande ninguém.
+- Lembre-se sempre, o seu foco determina a sua realidade.
+- Muito a aprender você ainda tem.
+"""
+
+        expected = {
+            "intents": [],
+            "actions": ["utter_star-wars"],
+            "templates": {
+                "utter_star-wars": [
+                    {
+                        "text": (
+                            "O medo leva à raiva, a raiva leva ao ódio"
+                            " e o ódio leva ao sofrimento."
+                        )
+                    },
+                    {"text": "Que a Força esteja com você!"},
+                    {
+                        "text": (
+                            "Grande guerreiro?"
+                            " Guerra não faz grande ninguém."
+                        )
+                    },
+                    {
+                        "text": (
+                            "Lembre-se sempre, o seu foco"
+                            " determina a sua realidade."
+                        )
+                    },
+                    {"text": "Muito a aprender você ainda tem."},
+                ]
+            },
+            "slots": [],
+            "entities": [],
+        }
+
+        lang.process(utters)
+        assert lang.domain == expected
+
     def test_domain_dump(self, lang, starwars_intent, tmpdir):
         lang.domain = {
             "intents": ["star-wars"],
             "actions": [],
-            "templates": [],
+            "templates": {},
             "slots": [],
             "entities": [],
         }
